08e630db2d94cfde05aa68711c2c370e33d33f0f
M-Rozell <95882154+M-Rozell@users.noreply.github.com>
2022-02-28 16:59:41 -0600

    got everything to line up

--- app.js
+++ app.js
@@ -6,34 +6,36 @@ $(document).ready(function () {
     var locations = ["St. Louis", "New Orleans", "Nashville", "Chattanooga", "Atlanta",
         "Athens", "Ashville", "Charlotte", "Bryson City", "Chicago"];
 
-    var wepons = ["Stick", "Coffee Cup", "RCP 90", "Springfield 9mm", "Coffee Cup",
+    var weapons = ["Stick", "Coffee Cup", "RCP 90", "Springfield 9mm", "Coffee Cup",
         "Sissors", "Fork", "Man O War", "Box Cutter", "Lung Cancer",
         "Rocks", "Pillow", "Rabid Possum", "Bottle Opener", "Plastic Bag",
         "Sock", "Expanding Foam", "Log", "Gallon of Worms", "The Void of Space"]
 
-    for (var i = 1; i < 101; i++) {
+    for (var i = 1; i <= 100; i++) {
         var $h3 = $("<h3>" + "Accusation " + [i] + ":" + "</h3>");
+
         $($h3).appendTo(".firstDiv").css({
             "cursor": "pointer",
             "user-select": "none"
         });
 
-        $h3.click(function () {
-            return thingsForMessage(i++);
-        })
+        $h3.click(thingsForMessage(i))
     }
-            
-    function thingsForMessage() {
-        var friend = friends[i % 5];
-        var location = locations[i % 10];
-        var wepon = wepons[i % 20]
 
-        function message() {
-            alert("I accuse " + friend + ", with the " + wepon + " in the " + location + "!");
-        }
-        return message();
+    function thingsForMessage(j) {
+        
+        var friend = friends[j % 5];
+        var location = locations[j % 10];
+        var weapon = weapons[j % 20];
+
+         function message () {
+            alert("Accusation " + [j] + ":" + " I accuse " + friend + ", with the " + weapon + " in " + location + "!");
+        };
+        return message;
     }
 });
 
-//thingsForMessage(i);
-//Accusation i: I accuse FRIEND_NAME, with the WEAPON_NAME in the LOCATION_NAME!
+
+
+
+
